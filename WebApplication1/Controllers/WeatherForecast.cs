be92bb46c16ff83fd33ed1c6a19641657c45b73f
Tshepiso37 <91364465+Tshepiso37@users.noreply.github.com>
2023-08-29 20:43:10 +0200

    WeatherForecast
    Created WeatherForecast controller, and added relevant code.

---
+++ WebApplication1/Controllers/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecast
+    {
+        
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
+
+        };
+        private readonly ILogger<WeatherForecast> _logger;
+    }
+}
