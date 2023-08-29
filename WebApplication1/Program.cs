e9d4fed1eb05e990aa3f95b446664691b413d466
Tshepiso37 <91364465+Tshepiso37@users.noreply.github.com>
2023-08-29 20:06:22 +0200

    Pipeline
    Configure HTTP request pipeline

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -109,20 +109,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else 
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "EcoPowerSolutions");
-        options.RoutePrefix = string.Empty;
-    }
-    );
-}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+IConfiguration configuration = app.Configuration;
+IWebHostEnvironment environment = app.Environment;
+
 
 app.MapControllers();
 
